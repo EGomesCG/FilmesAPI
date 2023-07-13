e803d99554890c20beefdf2b2e99db8862ce7b42
Erica Goncalves <erica.goncalves@alumisoft.com.br>
2023-07-13 10:11:11 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,34 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace FilmesAPI.Controllers;
+
+//As [] são anotações:
+[ApiController] //API de acesso
+[Route("[controller]")] //Rota de acesso do usuário a classe, tenho que passa o nome da classe dentro dos cochetes
+public class FilmeController : ControllerBase
+{
+    //Criando um objeto de lista de filme estático
+    private static List<Filme> filmes = new List<Filme>();
+
+    //Como estamos add informação a aplicação então a funcionalidade utilizada será Post
+    [HttpPost]//Verbo - Salvar
+    //Metodo pronto para cadastrar filme no nosso sistema,
+    //Se estamos cadastrando então o usuário precisa passar as informações para a API
+    //[FromBody] -> as informções vem do corpo da requisição
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
+    [HttpGet]//Verbo - Consultar
+    //Quando precisar consultar os dados, crio o metodo de busca do tipo LISTA DE FILME
+    //Como lista possui várias dependências uma delas é a IEnumerable, então,
+    //o ideal é alterar pois se a lista futuramente sofre alguma alteração na implementação, ou seja,
+    //deixa de se lista e se torne outra classe que tbém utilize o IEnumerable, então, não iremos precisar trocar o cabeçalho(tipo) do método
+    //Quanto menos dependemos de classe concreta no cabeçalho melhor será na implementação
+    public IEnumerable<Filme> RecuperarFilmes()
+    {
+        return filmes;
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+public class Filme
+{
+    //Posso colocar em cima dos campos as validações que quero que aconteça
+    [Required(ErrorMessage = "O título do filme é obrigatório")]//Obrigatório - Add mensagem entre parentes
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O genero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage ="O tamanho do genero não pode exceder 50 caracteres")]//Definido limite de caracteres
+    public string Genero { get; set; }
+    [Required]
+    [Range(70, 600, ErrorMessage ="A duração deve ter 70 e 600minutos")]//Estipulando minimo e máximo 
+    public int Duracao { get; set; }
+    public string Diretor { get; set; }
+}
+//A anotação[Required] torna obrigatório passar um parâmetro determinado.
+//A anotação[StringLength] limita o tamanho de caracteres de uma string.
+//A anotação [Range] limita o intervalo inferior e superior para valores numéricos.
